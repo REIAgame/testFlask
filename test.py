b27eae8552995a555d70ddd42f44a69a43e6c1d8
REIAgame <keishirou0@gmail.com>
2019-11-23 06:54:27 +0000

    test2

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ def postShow():
     return "<h1>{}</h1>".format(request.form["shows"])
 @app.route("/<name>", methods=["GET", "POST"])
 def namepage(name):
-    return render_template("name.html")
+    return render_template("name.html",name=name)
 
 if __name__ =="__main__":
     app.run(host="0.0.0.0",port=3000)
